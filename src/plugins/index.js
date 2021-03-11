cebf19ba3c32fdb0b3533262013a1b9475f11244
Сергей Фролов <frolov.frontend@gmail.com>
2021-03-11 12:52:28 +0300

    WIP: unused files

--- src/plugins/index.js
+++
@@ -1,9 +0,0 @@
-import { apply as applyAntDesignVue } from './ant-ui'
-import bemCssModules from './bem'
-// import { apply as applyStyledDirective } from './styled'
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const applyPlugins = ({ app }) => {
-  applyAntDesignVue(app)
-  app.use(bemCssModules)
-}
--- src/plugins/vue-tailwind/index.js
+++
@@ -1,7 +0,0 @@
-import plugin from 'vue-tailwind'
-const settings = {}
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const apply = (app) => {
-  app.use(plugin, settings)
-}
