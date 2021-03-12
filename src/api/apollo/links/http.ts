8be7442ed947c3306d860fa0567c447e7ccb7632
Сергей Фролов <frolov.frontend@gmail.com>
2021-03-13 01:11:28 +0300

    Upd: apollo client infrastructure refactored

--- src/api/apollo/links/http.ts
+++ src/api/apollo/links/http.ts
@@ -1,8 +1,5 @@
-// import { getEnv } from '@/common/tools'
 import { createHttpLink } from '@apollo/client'
-
 const httpLink = createHttpLink({
-  // You should use an absolute URL here
   uri: 'https://rickandmortyapi.com/graphql',
 })
 
--- src/store/local-state.ts
+++ src/store/local-state.ts
@@ -1,10 +1,6 @@
-import { useEventHook } from '@vue/apollo-composable/dist/util/useEventHook'
 import { ReactiveVar } from '@apollo/client/cache/inmemory/reactiveVars'
 import { makeVar, TypePolicies } from '@apollo/client'
 import { typePolicies } from '@/api/apollo'
-
-// export const extendCacheTypePoliciesEventHook = useEventHook()
-// export const typePoliciesExtenders = new Set()
 
 export const useStoreVariable = <T>({
   extendTypePolicies,
