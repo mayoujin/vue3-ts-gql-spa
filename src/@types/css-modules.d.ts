707a89ba972626bc0a472f6603f322cb1d819aa9
Сергей Фролов <frolov.frontend@gmail.com>
2021-03-13 01:13:53 +0300

    Upd: css modules declaration

--- src/@types/css-modules.d.ts
+++ src/@types/css-modules.d.ts
@@ -2,3 +2,8 @@ declare module '*.module.pcss' {
   const content: Record<string, string>
   export = content
 }
+
+declare module '*.shadow.pcss' {
+  const content: Record<string, string>
+  export = content
+}
