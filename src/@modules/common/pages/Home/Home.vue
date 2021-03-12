036dbf7b9533cd424ecb743778af3b20b8489bd2
Сергей Фролов <frolov.frontend@gmail.com>
2021-03-12 16:53:14 +0300

    WIP: Home page layout template code improved

--- src/@modules/common/pages/Home/Home.vue
+++ src/@modules/common/pages/Home/Home.vue
@@ -1,24 +1,22 @@
 <template>
-  <div v-bem>
-    <div v-bem:top>
-      <div v-bem:top-wrapper>
-        <h2 v-bem:title>
-          The quick, brown fox<br class="br" />
-          jumps over a lazy dog
-        </h2>
-        <p v-bem:desc>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.
-          explicabo.
+  <div :class="root()">
+    <div :class="[top(), root('top')]">
+      <div :class="top('wrapper')">
+        <h2 :class="top('title')">Sample VueJS 3-based project</h2>
+        <p :class="top('desc')">
+          This project aims to show core code sample, coding style and skills
+          and technologies applying.
         </p>
-        <div v-bem:more>
-          <a href="/" v-bem:more-link> Learn more </a>
+
+        <div :class="top('more')">
+          <a href="/" :class="top('more-link')"> Learn more </a>
         </div>
-        <div v-bem:social-links>
-          <div v-bem:social-links-follow>Follow us</div>
-          <div v-bem:links-wrapper>
-            <div v-bem:link-wrapper>
-              <a href="/" v-bem:link>
+
+        <div :class="links()">
+          <div :class="links('follow')">Follow us</div>
+          <div :class="links('list')">
+            <div :class="links('item')">
+              <a href="/" :class="links('link')">
                 <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
                   <path
                     d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z"
@@ -26,8 +24,8 @@
                 </svg>
               </a>
             </div>
-            <div v-bem:link-wrapper>
-              <a href="/" v-bem:link>
+            <div :class="links('item')">
+              <a href="/" :class="links('link')">
                 <svg viewBox="0 0 30 30" fill="currentColor" class="h-6">
                   <circle cx="15" cy="15" r="4"></circle>
                   <path
@@ -36,8 +34,8 @@
                 </svg>
               </a>
             </div>
-            <div v-bem:link-wrapper>
-              <a href="/" v-bem:link>
+            <div :class="links('item')">
+              <a href="/" :class="links('link')">
                 <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
                   <path
                     d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z"
@@ -45,8 +43,8 @@
                 </svg>
               </a>
             </div>
-            <div v-bem:link-wrapper>
-              <a href="/" v-bem:link>
+            <div :class="links('item')">
+              <a href="/" :class="links('link')">
                 <svg viewBox="0 0 24 24" fill="currentColor" class="h-6">
                   <path
                     d="M23.8,7.2c0,0-0.2-1.7-1-2.4c-0.9-1-1.9-1-2.4-1C17,3.6,12,3.6,12,3.6h0c0,0-5,0-8.4,0.2 c-0.5,0.1-1.5,0.1-2.4,1c-0.7,0.7-1,2.4-1,2.4S0,9.1,0,11.1v1.8c0,1.9,0.2,3.9,0.2,3.9s0.2,1.7,1,2.4c0.9,1,2.1,0.9,2.6,1 c1.9,0.2,8.2,0.2,8.2,0.2s5,0,8.4-0.3c0.5-0.1,1.5-0.1,2.4-1c0.7-0.7,1-2.4,1-2.4s0.2-1.9,0.2-3.9v-1.8C24,9.1,23.8,7.2,23.8,7.2z M9.5,15.1l0-6.7l6.5,3.4L9.5,15.1z"
@@ -58,9 +56,10 @@
         </div>
       </div>
     </div>
-    <div v-bem:bottom>
+
+    <div :class="[root('bottom'), bottom()]">
       <img
-        v-bem:img
+        :class="bottom('img')"
         src="https://tsh.io/wp-content/uploads/2020/08/vue-cli-installation_.jpg"
       />
     </div>
@@ -68,17 +67,33 @@
 </template>
 
 <script lang="ts">
+import { useEmitMetadata } from '@/tools'
+import { metadata } from './config'
+
 import { defineComponent } from 'vue'
 import { useBem } from '@/plugins/bem'
 
-import { setup } from './setup'
 import styles from './styles.module.pcss'
+import _styles from './styles.module.pcss.d'
 
-const $bemCn = useBem<string>(styles)
+/**
+ * Class names generators
+ */
+const cn = {
+  root: useBem<keyof typeof _styles>(styles, 'root'),
+  top: useBem<keyof typeof _styles>(styles, 'top'),
+  bottom: useBem<keyof typeof _styles>(styles, 'bottom'),
+  links: useBem<keyof typeof _styles>(styles, 'social-links'),
+}
+
+const setup = (_, context) => {
+  useEmitMetadata(metadata, context)
+  return {
+    ...cn,
+  }
+}
 
 export default defineComponent({
   setup,
-  $bemCn,
 })
 </script>
-
